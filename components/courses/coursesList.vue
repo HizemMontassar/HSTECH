b8dc84ba9767185323bed2bd899949e4ea4b3466
hizem_montassar <44597417+HizemMontassar@users.noreply.github.com>
2023-01-17 18:20:49 +0100

    test: adding courses list layout

---
+++ components/courses/coursesList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex p-16">
+    <div class="w-full">
+      <CoursesCourseCard />
+      <CoursesCourseCard />
+      <CoursesCourseCard />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <NuxtLayout name="custom">
       <div class="flex flex-col w-full h-screen">
         <Cover />
-        <div></div>
+        <CoursesList />
       </div>
     </NuxtLayout>
   </div>
