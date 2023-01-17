61ffc4a1f311930317a91b187ae36f81f4808a1b
hizem_montassar <44597417+HizemMontassar@users.noreply.github.com>
2023-01-18 00:48:24 +0100

    style: changing courseCard layout

--- components/courses/courseCard.vue
+++ components/courses/courseCard.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-col md:flex-row items-center my-8 shadow-xl">
     <img :src="props.cours.url" alt="" class="object-cover h-full
     w-full sm:w-3/4 lg:w-1/2" />
-    <div class="flex-col md:flex-row px-4 text-center">
-      <h1 class="font-bold mb-4">{{ props.cours.name }} Course</h1>
+    <div class="flex-col md:flex-row px-4">
+      <h1 class="font-bold mb-4 text-center">{{ props.cours.name }} Course</h1>
       <p class="font-sans font-semibold">
         {{ props.cours.description }}
       </p>
--- data/courses.ts
+++ data/courses.ts
@@ -10,21 +10,21 @@ export const courses: Course[] = [
     id: 1,
     name: "Angular",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque. Vestibulum tristique condimentum tortor, vel condimentum leo rhoncus non. Aenean quis posuere lectus. Etiam purus sapien, fringilla vel dictum quis, hendrerit at odio.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque.",
     url: "https://miro.medium.com/max/720/1*eOE7VhXBlqdIJ9weEdHbQQ.webp",
   },
   {
     id: 2,
-    name: "Reactjs",
+    name: "Vue.js",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque. Vestibulum tristique condimentum tortor, vel condimentum leo rhoncus non. Aenean quis posuere lectus. Etiam purus sapien, fringilla vel dictum quis, hendrerit at odio.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque.",
     url: "https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png",
   },
   {
     id: 3,
-    name: "Vue.js",
+    name: "ReactJs",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque. Vestibulum tristique condimentum tortor, vel condimentum leo rhoncus non. Aenean quis posuere lectus. Etiam purus sapien, fringilla vel dictum quis, hendrerit at odio.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur elit ligula, faucibus eget nibh ut, hendrerit gravida neque.",
     url: "https://cdn-media-1.freecodecamp.org/images/1*3WTslHrSuJfqlj3qZRRFPQ.png",
   },
 ];
