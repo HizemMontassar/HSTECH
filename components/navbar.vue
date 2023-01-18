1452e1692a548f16eb109500923cddc1327f4e71
hizem_montassar <44597417+HizemMontassar@users.noreply.github.com>
2023-01-18 01:17:24 +0100

    style: making navbar sticky

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row justify-between items-center w-full h-10 bg-white shadow-md">
+  <div class="flex flex-row justify-between items-center w-full h-10 bg-white shadow-md sticky top-0 z-10">
     <div class="mt-auto">
       <button>
         <svg
